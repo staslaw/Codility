a2b4c884707c7da7c515e53e1e0be122656f5185
Stanislaw <sniemyski@o2.pl>
2018-10-04 12:17:30 +0200

    Lesson 3 task 2 and 3

--- src/zadania/L3_Time_Complexity/TapeEquilibrium/Solution.java
+++ src/zadania/L3_Time_Complexity/TapeEquilibrium/Solution.java
@@ -28,12 +28,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Random random = new Random();
-        int N = random.nextInt(9) + 2;
+        int N = random.nextInt(99999) + 2;
         int[] tab = new int[N];
         for (int i = 0; i < tab.length; i++) {
-            tab[i] = random.nextInt(21) - 10;
+            tab[i] = random.nextInt(2001) - 1000;
         }
-        int[] tab1 = {2, 4, 3, 1, 5};
         System.out.println(Arrays.toString(tab));
         System.out.println(solution(tab));
     }
