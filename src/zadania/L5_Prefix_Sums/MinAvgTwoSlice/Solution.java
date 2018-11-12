2d8d1003733f898d15fd86816b71abcdbd39822f
Stanislaw <sniemyski@o2.pl>
2018-11-12 21:21:43 +0100

    Lesson 5 task 3 - still 60%

--- src/zadania/L5_Prefix_Sums/MinAvgTwoSlice/Solution.java
+++ src/zadania/L5_Prefix_Sums/MinAvgTwoSlice/Solution.java
@@ -1,16 +1,29 @@
 package zadania.L5_Prefix_Sums.MinAvgTwoSlice;
 
-import java.util.Arrays;
 
 public class Solution {
 
     public static int solution(int[] A) {
-        int resultIndex = A.length;
+        int resultIndex = A.length - 2;
+        double localSum = (double) (A[A.length -1] + A[A.length - 2]);
+        double localCount = 2d;
+        double globalAvg = localSum / localCount;
 
-        double[][] sums = new double[A.length - 1][];
+        for (int i = A.length - 3; i >= 0; i--) {
+            localSum = localSum + (double) A[i];
+            localCount = localCount + 1d;
+            double Avg1 = localSum / localCount;
+            double Avg2 = ((double) (A[i] + A[i + 1])) / 2d;
+            if (Avg2 <= Avg1) {
+                localSum = (double) (A[i] + A[i + 1]);
+                localCount = 2d;
 
-       
-
+                if (Avg2 <= globalAvg) {
+                    globalAvg = Avg2;
+                    resultIndex = i;
+                }
+            }
+        }
         return resultIndex;
     }
 
@@ -19,7 +32,6 @@ public class Solution {
         System.out.println(solution(tab));
     }
 }
-
 
 
 //    public static int solution(int[] A) {
@@ -48,6 +60,33 @@ public class Solution {
 //        System.out.println(Arrays.toString(minAvg));
 //
 //
+//        return resultIndex;
+//    }
+
+
+
+
+//    public static int solution(int[] A) {
+//        int resultIndex = 0;
+//        double localSum = (double) (A[0] + A[1]);
+//        double localCount = 2d;
+//        double globalAvg = localSum / localCount;
+//
+//        for (int i = 2; i < A.length; i++) {
+//            localSum = localSum + (double) A[i];
+//            localCount = localCount + 1d;
+//            double Avg1 = localSum / localCount;
+//            double Avg2 = ((double) (A[i] + A[i - 1])) / 2d;
+//            if (Avg2 <= Avg1) {
+//                localSum = (double) (A[i] + A[i - 1]);
+//                localCount = 2d;
+//
+//                if (Avg2 < globalAvg) {
+//                    globalAvg = Avg2;
+//                    resultIndex = i - 1;
+//                }
+//            }
+//        }
 //        return resultIndex;
 //    }
 
